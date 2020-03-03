05717e3e51c2be917d131f844781a649e031fbfd
William Lin <willlin1996@gmail.com>
2020-03-03 16:42:53 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -62,12 +62,8 @@ def apply_coupons(cart, coupons)
      cart_with_coupon[:count] += coupons[counter][:num]
      cart_with_coupon[:count] -= coupons[counter][:num]
    else 
-     cart_item_with_coupon = {
-       :item => couponed_item_name,
-       :price => couopons[counter][:cost] / coupons[counter][:num],
-       :count => coupons[counter][:num]
-       :clearance => cart_item[:clearance]
-     }
+     cart_item_with_coupon = {:item => couponed_item_name, :price => coupons[counter][:cost] / coupons[counter][:num], :count => coupons[counter][:num], :clearance => cart_item[:clearance]}
+     
      cart << cart_item_with_coupon
      car_item[:count] -= coupons[counter][:num]
    end
