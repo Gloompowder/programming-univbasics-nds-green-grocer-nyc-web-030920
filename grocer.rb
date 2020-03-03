e1f53eafd4f8533d3c19a13a594701fd577045bb
William Lin <willlin1996@gmail.com>
2020-03-03 16:16:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -69,10 +69,24 @@ def checkout(cart, coupons)
     cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart) 
     coupon_item_name = "#{coupons[counter][:item]} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name)
-    if cart_item && cart_item[:count] == coupons[counter][:num]
+    if cart_item && cart_item[:count] >= coupons[counter][:num]
+      if cart_item_with_coupon 
+        cart_item_with_coupon[:count] += coupons[counter][:num]
+        cart_item[:count] -= coupons[counter][:num]
+      else 
+        cart_item_with_coupon = { 
+          :item => coupon_item_name,
+          :price => coupon[counter][:cost] / coupon[counter][:num]
+          :clearance => cart_item[:clearance]
+          :count => coupons[counter][:num]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupon[counter][:num]
+      end
     end
     counter += 1
   end  
+  cart
   # This method should call
   # * consolidate_cart
   # * apply_coupons
