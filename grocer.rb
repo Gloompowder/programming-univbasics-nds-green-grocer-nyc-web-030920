a0bae4070e547ffad0a563d0e59d63514e3ea96b
William Lin <willlin1996@gmail.com>
2020-03-03 17:03:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -92,7 +92,7 @@ def checkout(cart, coupons)
   # Consult README for inputs and outputs
   #
   consolidated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidated_cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   
   total = 0
